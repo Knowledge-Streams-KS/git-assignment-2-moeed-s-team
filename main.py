421d765c5846da688d1e3365a8959eae83484d5b
angrabdulmoeed <getmoeed@gmail.com>
2023-01-09 14:58:34 +0500

    created main.py file with inputs from user

---
+++ main.py
@@ -0,0 +1,25 @@
+n1 = input("Enter first number: ")
+n2 = input("\nEnter second number: ")
+
+print ("\n1-Add\n 2-Subtract\n 3-Multiply\n 4-Divide\n 5-Modulus\n 6-Square Root\n")
+
+fun = input("Enter operation:")
+
+if (fun == 1){
+
+}
+if (fun == 2){
+    
+}
+if (fun == 3){
+    
+}
+if (fun == 4){
+    
+}
+if (fun == 5){
+    
+}
+if (fun == 6){
+    
+}
