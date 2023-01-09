197c9427400cdda5cb8f01cb0d4eba7206c41773
angrabdulmoeed <getmoeed@gmail.com>
2023-01-09 23:21:08 +0500

    added modulus function into main file and commented code

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-n1 = input("Enter first number: ")
-n2 = input("\nEnter second number: ")
+from modulus import * #importing the modulus function defined in modulus.py
 
-print ("\n1-Add\n 2-Subtract\n 3-Multiply\n 4-Divide\n 5-Modulus\n 6-Square Root\n")
+n1 = input("Enter first number: ") #get the first number from user and store it in n1 variable
+n2 = input("\nEnter second number: ") #get the second number from user and store it in n2 variable
 
-fun = input("Enter operation:")
+print ("\nPress number according to desired function \n1-Add\n 2-Subtract\n 3-Multiply\n 4-Divide\n 5-Modulus\n 6-Square Root\n") #print the available calculator operations
+
+fun = input("Enter operation: ") #get the user input for operation to be performed
 
 if fun == 1:
-
+    add(n1, n2)
 
 elif fun == 2:
-    
+    subtract(n1, n2)
 
 elif fun == 3:
-    
+    multiply(n1, n2)
 
 elif fun == 4:
-    
+    divide(n1, n2)
 
 elif fun == 5:
-    
+    modulus(n1, n2)
 
 elif fun == 6:
-    
+    sq_root(n1, n2) #calling the square root function
