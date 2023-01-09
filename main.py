ffb83ff295e0d3a832ee8290735988294acbd7f7
angrabdulmoeed <getmoeed@gmail.com>
2023-01-09 15:01:51 +0500

    fixed format of else-if conditions

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ print ("\n1-Add\n 2-Subtract\n 3-Multiply\n 4-Divide\n 5-Modulus\n 6-Square Root
 
 fun = input("Enter operation:")
 
-if (fun == 1){
+if fun == 1{
 
 }
-if (fun == 2){
+elif fun == 2{
     
 }
-if (fun == 3){
+elif fun == 3{
     
 }
-if (fun == 4){
+elif fun == 4{
     
 }
-if (fun == 5){
+elif fun == 5{
     
 }
-if (fun == 6){
+elif fun == 6{
     
 }
