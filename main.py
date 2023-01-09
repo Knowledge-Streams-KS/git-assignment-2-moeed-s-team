de21febdfac9af040d772372845eaa70323c6be3
angrabdulmoeed <getmoeed@gmail.com>
2023-01-09 15:04:25 +0500

    fixed else if format by removing brackets

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ print ("\n1-Add\n 2-Subtract\n 3-Multiply\n 4-Divide\n 5-Modulus\n 6-Square Root
 
 fun = input("Enter operation:")
 
-if fun == 1{
+if fun == 1:
 
-}
-elif fun == 2{
+
+elif fun == 2:
     
-}
-elif fun == 3{
+
+elif fun == 3:
     
-}
-elif fun == 4{
+
+elif fun == 4:
     
-}
-elif fun == 5{
+
+elif fun == 5:
     
-}
-elif fun == 6{
+
+elif fun == 6:
     
-}
