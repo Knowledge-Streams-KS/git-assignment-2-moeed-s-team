91e95535fa3336eae5f002ddea3bde3090b6b1f0
angrabdulmoeed <getmoeed@gmail.com>
2023-01-09 23:23:46 +0500

    created sq_root.py file and function

---
+++ sq_root.py
@@ -0,0 +1,9 @@
+import math # import the math module
+
+def sq_root(num1, num2):
+    num1_sqrt = math.sqrt(num1) 
+    num2_sqrt = math.sqrt(num2) #taking square root of both numbers and storing it in different variables
+
+    
+    print(f"Square root of {num1} = {num1_sqrt}")
+    print(f"Square root of {num2} = {num2_sqrt}")
